d57e190b1d667372325b18217c81dcaea769d47f
AMINE.ZOUHAID <amine2020zouhaid@gmail.com>
2022-03-10 17:25:40 +0100

    add files

--- ApplicationAngulair/src/app/compenents/uploadfiles/uploadfiles.component.ts
+++ ApplicationAngulair/src/app/compenents/uploadfiles/uploadfiles.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-uploadfiles',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uploadfiles.component.css']
 })
 export class UploadfilesComponent implements OnInit {
-
-  constructor() { }
+  public uploadForm!:FormGroup;
+  constructor(private formbuilder:FormBuilder,private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+
+    this.uploadForm=this.formbuilder.group({
+      file:[''],
+      
+    })
   }
 
+
+  upload(){
+    this.http.post<any>("http://localhost:3000/upload",this.uploadForm.value).subscribe(res=>{
+alert("Files is upload");
+this.uploadForm.reset();
+this.router.navigate(['uploadfiles']);
+    },err=>{
+      alert("Semething went wrong ");
+    })
+
+}
 }
